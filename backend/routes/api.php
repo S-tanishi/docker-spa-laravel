a53cdc5e69d0fb6c2aa65e9be749d9af29051351
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-26 19:45:37 +0900

    add:認証処理

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\Admin\Auth\LoginController;
 use Illuminate\Http\Request;
+use Illuminate\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Admin\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// 管理画面の機能として実装するためPrefixにadminをつける
+Route::prefix('admin')->group(function() {
+    // 認証処理
+    Route::prefix('ayth')->group(function () {
+        Route::post(('/login', [LoginController::class, 'login']);
+        });
+
+        Route::middleware('auth:api')->group(function () {
+            Route::get('/user', function () {
+                return Auth::user();
+            });
+
+            // Users
+            Route::resource('users', UserContorller::class)->only([
+                'index']);
+        });
 });
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
