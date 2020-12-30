eb23e96f01731284b0affba41b3c34597a075e13
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-30 18:04:09 +0900

    add:API認証　ルーティング

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Api\Admin\UserController;
+use App\Http\Controllers\Api\Admin\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// 管理画面の機能として実装するのでPrefixにadminをつける
+Route::prefix('admin')->group(function() {
+    // 認証処理
+    Route::prefix('auth')->group(function () {
+        Route::post('/login', [LoginController::class, 'login']);
+    });
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/user', function () {
+            return Auth::user();
+        });
+
+        // Users
+        Route::resource('users', UserController::class)->only([
+            'index'
+        ]);
+    });
 });
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
