2112d6b9e2a86b22bd416fb00d936cbd0b6a4bd3
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-30 18:00:25 +0900

    add:corsのorigin設定を同一ネットワークのホスト名に変更

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://web',
+    ],
+//  'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
